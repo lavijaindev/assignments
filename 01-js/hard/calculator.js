4f11d7f86e9bf575fa7027d06b3231d842694da5
Lavi Jain <67602004+lavijaindev@users.noreply.github.com>
2023-12-09 02:04:12 +0530

    #solution calculator.js

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,51 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+
+  add(number) {
+    this.result += number;
+  }
+
+  subtract(number) {
+    this.result -= number;
+  }
+
+  multiply(number) {
+    this.result *= number;
+  }
+
+  divide(number) {
+    if (number === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    this.result /= number;
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  calculate(expression) {
+    // Remove continuous spaces and validate expression
+    const sanitizedExpression = expression.replace(/\s+/g, '').replace(/[^0-9+\-*/().]/g, '');
+    
+    try {
+      this.result = eval(sanitizedExpression);
+      if (!isFinite(this.result)) {
+        throw new Error("Invalid result");
+      }
+    } catch (error) {
+      throw new Error("Invalid expression");
+    }
+  }
+}
 
 module.exports = Calculator;
