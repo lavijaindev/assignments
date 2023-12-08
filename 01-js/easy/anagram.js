c38fe3714bc10946a7d5de7f61e0d2d9591011d1
Lavi Jain <67602004+lavijaindev@users.noreply.github.com>
2023-12-09 02:00:49 +0530

    #solution anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -4,8 +4,25 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
-function isAnagram(str1, str2) {
+function removeWhiteSpaceFromString(str) {
+  return str.replace(/[^a-zA-Z]/g, '');
+}
 
+function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  cleanStr1 = removeWhiteSpaceFromString(str1.toLowerCase());
+  cleanStr2 = removeWhiteSpaceFromString(str2.toLowerCase());
+  str1 = cleanStr1.split("").sort();
+  str2 = cleanStr2.split("").sort();
+
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] !== str2[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
