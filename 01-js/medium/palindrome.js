8193c81820dcb56e85dfdc31a997a3cf1f1673fd
Lavi Jain <67602004+lavijaindev@users.noreply.github.com>
2023-12-09 02:03:18 +0530

    #solution palindrome.js

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -3,8 +3,22 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
+function removeNonAlphanumericChars(str) {
+  return str.replace(/[^a-zA-Z]/g, '');
+}
+
 function isPalindrome(str) {
-  return true;
+  // Remove non-alphabetic characters and convert to lowercase
+  let cleanedStr = removeNonAlphanumericChars(str.toLowerCase());
+
+  let strArr = cleanedStr.split("");
+  let newStrArr = strArr.slice().reverse();
+
+  let palindromeStr1 = strArr.join("");
+  let palindromeStr2 = newStrArr.join("");
+
+  // Check if the strings are equal (palindromes)
+  return palindromeStr1 === palindromeStr2;
 }
 
 module.exports = isPalindrome;
