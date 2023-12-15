5c4ca0d248258fdcfbcadcb879d556e7d5604f99
Lavi Jain <67602004+lavijaindev@users.noreply.github.com>
2023-12-16 00:38:43 +0530

    Update 1-promisify-setTimeout.js

--- week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,6 +3,25 @@
 */
 
 function wait(n) {
+    const time = n * 1000;
+    let p = new Promise(function(resolve){
+        setTimeout(() => {
+            // let a = 0;
+            // for(let i=0; i< 5; i++) {
+            //     a = a+i;
+            // }
+            // resolve(a);
+            resolve();
+        },time);
+    });
+    return p;
+
 }
+
+wait(1).then((result) => {
+    console.log(result);
+}).catch((err) => {
+    console.log(err);
+});
 
 module.exports = wait;
